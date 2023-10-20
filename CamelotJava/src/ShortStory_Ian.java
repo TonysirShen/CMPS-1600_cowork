88a20b8a2bad12280d92ae33a0af095f4d183542
iankreger <iankreger@localhost>
2023-10-20 11:45:00 -0500

    Action Sequences

--- CamelotJava/src/ShortStory_Ian.java
+++ CamelotJava/src/ShortStory_Ian.java
@@ -16,9 +16,9 @@ public class ShortStory_Ian implements IStory{
 	
 	
 	private enum NodeLabels{
-		Start, Init, Cottage, City, CastleCrossRoad, Getsword, Sword_city, swordBlackSmith, TalktoBlacksmith2,
-		gethelmet2, successSpookyRoad, BanditSubmit, Dungeon, FightWithWarlock, Blacksmith, getHelmet, helmetCity, 
-		helmetCrossroad, helmetGreatHall, helmetGetSword, failSpookyRoad, BanditWin, GuardCamp,GetSwordHelmet, 
+		Start, Init, Cottage, City, CastleCrossRoad, Getsword, Sword_city, SwordBlackSmith, TalktoBlacksmith2,
+		GetHelmet2, SuccessSpookyRoad, BanditSubmit, Dungeon, FightWithWarlock, Blacksmith, GetHelmet, HelmetCity, 
+		HelmetCrossroad, HelmetGreatHall, HelmetGetSword, FailSpookyRoad, BanditWin, GuardCamp,GetSwordHelmet, 
 		GameOVER, GreatHall	
 	}
 	
@@ -55,5 +55,30 @@ public class ShortStory_Ian implements IStory{
 		return null;
 	}
 	private ActionSequence getStartSQ() {return null;}
+	private ActionSequence getCitySQ() {return null;}
+	private ActionSequence getCottageSQ() {return null;}
+	private ActionSequence getCastleCrossRoadSQ() {return null;}
+	private ActionSequence getGetSwordSQ() {return null;}
+	private ActionSequence getSword_CitySQ() {return null;}
+	private ActionSequence getSwordBlackSmithSQ() {return null;}
+	private ActionSequence getTalktoBlacksmithSQ() {return null;}
+	private ActionSequence getGetHelmet2SQ() {return null;}
+	private ActionSequence getSuccessSpookyRoadSQ() {return null;}
+	private ActionSequence getBanditSubmitSQ() {return null;}
+	private ActionSequence getDungeonSQ() {return null;}
+	private ActionSequence getFightWithWarlockSQ() {return null;}
+	private ActionSequence getBlacksmithSQ() {return null;}
+	private ActionSequence getGetHelmetSQ() {return null;}
+	private ActionSequence getHelmetCitySQ() {return null;}
+	private ActionSequence getHelmetCrossroadSQ() {return null;}
+	private ActionSequence getHelmetGreatHallSQ() {return null;}
+	private ActionSequence getHelmetGetSwordSQ() {return null;}
+	private ActionSequence getFailSpookyRoadSQ() {return null;}
+	private ActionSequence getBanditWinSQ() {return null;}
+	private ActionSequence getGuardCampSQ() {return null;}
+	private ActionSequence getGetSwordHelmetSQ() {return null;}
+	private ActionSequence getGameOVERSQ() {return null;}
+	private ActionSequence getGreatHallSQ() {return null;}
 	
+		
 }
