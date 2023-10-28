2a13b101e110d78ce1f5ffc3d0a137143013b6eb
TS539 <TS539@LAPTOP-5BQ5FMC1>
2023-10-28 15:53:48 -0500

    increase bandit number to 3

--- CamelotJava/src/com/entities/IThing.java
+++ CamelotJava/src/com/entities/IThing.java
@@ -2,7 +2,7 @@ package com.entities;
 
 public interface IThing<T> {
 	public enum ThingNames{
-		/* Characters*/jojo,guard,king,blacksmith,warlock,bandit,tom, merchant, beggar,
+		/* Characters*/jojo,guard,king,blacksmith,warlock,bandit1,bandit2,bandit3,tom, merchant, beggar,
 		/* Items*/sword,helmet,Greenpotion, Bluecloth, Coin,
 		/*Places*/home,city,camp,castlecrossroad,GreatHall,Spookypath,jail, Port,Ruins,BShouse
 	} 
