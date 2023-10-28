185fb56909024d0b24ac9902f432e77ad89d6007
TS539 <TS539@LAPTOP-5BQ5FMC1>
2023-10-28 16:16:09 -0500

    Set HeacyArmor Clothing, add three hammer as weapon for bandit

--- CamelotJava/src/com/entities/IThing.java
+++ CamelotJava/src/com/entities/IThing.java
@@ -3,7 +3,7 @@ package com.entities;
 public interface IThing<T> {
 	public enum ThingNames{
 		/* Characters*/jojo,guard,king,blacksmith,warlock,bandit1,bandit2,bandit3,tom, merchant, beggar,
-		/* Items*/sword,helmet,Greenpotion, Bluecloth, Coin,
+		/* Items*/sword,helmet,Greenpotion, Bluecloth, Coin,Hammer1,Hammer2,Hammer3,
 		/*Places*/home,city,camp,castlecrossroad,GreatHall,Spookypath,jail, Port,Ruins,BShouse
 	} 
 	public String getName();
