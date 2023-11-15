a6f915c8ef6995456ed20fb54dfc60940ebbf835
TS539 <TS539@10.129.113.43>
2023-11-15 11:45:50 -0600

    Wait method

---
+++ CamelotJava/src/Wait.java
@@ -0,0 +1,25 @@
+import com.actions.IAction;
+
+public class Wait implements IAction {
+	private int time;
+	
+	public Wait(int a) {
+		this.time = a;
+	}
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Wait";
+	}
+
+	@Override
+	public boolean getShouldWait() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s(%d)", getName(),this.time);
+	}
+}
