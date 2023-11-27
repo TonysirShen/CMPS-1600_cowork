de8a3896f87b8c26a69c5edc7f46bf3499653c1d
TS539 <TS539@LAPTOP-5BQ5FMC1>
2023-11-26 23:23:04 -0600

    Change Die to Kneel cuz died character cannot receive input

--- CamelotJava/src/com/actions/Kneel.java
+++ CamelotJava/src/com/actions/Kneel.java
@@ -2,21 +2,21 @@ package com.actions;
 
 import com.entities.Character;
 
-public class Die implements IAction {
+public class Kneel implements IAction {
 	Character character;
 	boolean died;
-	public Die(Character c) {
+	public Kneel(Character c) {
 		this.character =c;
 		this.died = true;
 	}
-	public Die(Character c, boolean d) {
+	public Kneel(Character c, boolean d) {
 		this.character = c;
 		this.died = d;
 	}
 	@Override
 	public String getName() {
 		if (died){// TODO Auto-generated method stub
-			return "Die";
+			return "Kneel";
 		}
 		else {
 			return"Revive";
